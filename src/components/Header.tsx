27ef6be7092f52cedcd09f6ce5e9323b2d149f07
Diego Masin <diegoifce@gmail.com>
2022-01-07 23:33:14 -0300

    Limpando o template.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
-import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
 import {
-  RiSearchLine,
-  RiNotificationLine,
-  RiUserAddLine,
-} from 'react-icons/ri';
+  Flex,
+  Text,
+  Icon,
+  HStack,
+  Box,
+  Avatar,
+  Image,
+  Link,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { RiNotificationLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header() {
   return (
@@ -24,32 +30,12 @@ export function Header() {
         w="64"
         color="yellow.500"
       >
-        Binance
+        <NextLink href="/dashboard" passHref>
+          <Link>
+            <Image src="/images/logo-horizontal.png" alt="Logo" height={50} />
+          </Link>
+        </NextLink>
       </Text>
-
-      {/* <Flex
-        as="label"
-        flex="1"
-        py="4"
-        px="8"
-        ml="6"
-        maxWidth={400}
-        alignSelf="center"
-        color="gray.200"
-        position="relative"
-        bg="gray.800"
-        borderRadius="full"
-      >
-        <Input
-          color="gray.50"
-          variant="unstyled"
-          px="4"
-          mr="4"
-          placeholder="Search in plattform"
-          _placeholder={{ color: 'gray.400' }}
-        />
-        <Icon as={RiSearchLine} fontSize="20" />
-      </Flex> */}
 
       <Flex align="center" ml="auto">
         <HStack
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,67 +1,55 @@
-import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react';
-import dynamic from 'next/dynamic';
+import { Box, Flex, Grid, GridItem, SimpleGrid, Text } from '@chakra-ui/react';
 import { Header } from '../components/Header';
-import { Sidebar } from '../components/Sidebar';
-
-const Chart = dynamic(() => import('react-apexcharts'), {
-  ssr: false,
-});
-
-const options = {
-  chart: { toolbar: { show: false }, zoom: { enabled: false } },
-  foreColor: theme.colors.gray[500],
-  grid: { show: false },
-  dataLabels: { enabled: false },
-  tooltip: { enabled: false },
-  // xaxis: {
-  //   type: 'datetime',
-  //   axisBorder: { color: theme.colors.gray[600] },
-  //   axisTicks: { color: theme.colors.gray[600] },
-  //   categories: [
-  //     '2021-03-18T00:00:00.000Z',
-  //     '2021-03-19T00:00:00.000Z',
-  //     '2021-03-20T00:00:00.000Z',
-  //     '2021-03-21T00:00:00.000Z',
-  //     '2021-03-22T00:00:00.000Z',
-  //     '2021-03-23T00:00:00.000Z',
-  //     '2021-03-24T00:00:00.000Z',
-  //   ],
-  // },
-  fill: {
-    opacity: 0.3,
-    type: 'gradient',
-    gradient: {
-      shade: 'dark',
-      opacityFrom: 0.7,
-      opacityTo: 0.3,
-    },
-  },
-};
-
-const series = [{ name: 'series1', data: [15, 20, 39, 58, 70, 30, 12] }];
 
 export default function Dashboard() {
   return (
     <Flex direction="column" h="100vh">
       <Header />
-      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-        <Sidebar />
-
-        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
-            <Text fontSize="lg" mb="4">
-              Inscritos da Semana
-            </Text>
-            <Chart options={options} series={series} type="area" height={160} />
-          </Box>
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
-            <Text fontSize="lg" mb="4">
-              Taxa de abertura
-            </Text>
-            <Chart options={options} series={series} type="area" height={160} />
-          </Box>
-        </SimpleGrid>
-      </Flex>
+      <Grid
+        width="100%"
+        maxWidth={1480}
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(4, 1fr)"
+        my="6"
+        mx="auto"
+        px="6"
+      >
+        <GridItem
+          w="100%"
+          h="10"
+          colSpan={3}
+          bg="gray.800"
+          borderColor="gray.700"
+          borderWidth="1px"
+        ></GridItem>
+        <GridItem
+          w="100%"
+          h="20"
+          bg="gray.800"
+          colSpan={1}
+          rowSpan={2}
+          borderColor="gray.700"
+          borderWidth="1px"
+        >
+          Teste
+        </GridItem>
+        <GridItem
+          w="100%"
+          h="10"
+          bg="gray.800"
+          colSpan={1}
+          borderColor="gray.700"
+          borderWidth="1px"
+        ></GridItem>
+        <GridItem
+          w="100%"
+          h="10"
+          bg="gray.800"
+          colSpan={2}
+          borderColor="gray.700"
+          borderWidth="1px"
+        ></GridItem>
+      </Grid>
     </Flex>
   );
 }
